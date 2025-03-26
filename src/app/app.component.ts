80444f213e533b7888f664afba352d3658cee775
bmpmd <sk8leo@live.com>
2025-03-26 14:32:50 -0400

    computed signal for searching items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, effect, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComputedComponentComponent } from './computed-component/computed-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComputedComponentComponent],
+  imports: [RouterOutlet, CommonModule, ComputedComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,6 +14,34 @@ export class AppComponent implements OnInit{
   title = 'signal-practice';
 
   theme = signal('light');
+
+  
+
+  //product list 
+  products = signal([
+    {id: 1, name: "milk", price:1.50},
+    {id: 2, name: "eggs", price:9.50},
+    {id: 3, name: "bread", price:2.50},
+  ])
+
+  //filtername signal
+  filterName = signal('')
+
+  //filtered products computed signal 
+  filteredProducts = computed(()=>{
+    return this.products()
+    .filter(product => product.name
+      .toLowerCase()
+      .includes(this.filterName().toLowerCase())
+     )
+  })
+
+  //change filter method
+  //get filtername on change 
+  changeFilter(event: Event){
+    let newFilter = (event.target as HTMLInputElement).value
+    this.filterName.set(newFilter)
+  }
 
   label = this.theme()
   ngOnInit(): void {
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+
+
+}
