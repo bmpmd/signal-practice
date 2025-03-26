ccf5c4f789bf787a0d10c5877e5be550f9e163d7
bmpmd <sk8leo@live.com>
2025-03-26 13:53:50 -0400

    creating a  signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'signal-practice';
+
+  theme = signal('light');
+
+  ngOnInit(): void {
+      document.body.className = this.theme()
+  }
+
+
 }
